ba4a99d83c1c1535a6cde8598fa0349821a66bd0
Jasman Singh Sahi <jasman.singh.sahi@gmail.com>
2023-11-08 23:24:22 -0500

    golang table works color left

--- todo/todo.go
+++ todo/todo.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"os"
 	"time"
+
+	"github.com/alexeyco/simpletable"
 )
 
 type item struct {
@@ -83,8 +85,40 @@ func (t *Todos) Store(fileName string) error {
 }
 
 func (t *Todos) Print() {
+	//for i, item := range *t {
+	//	fmt.Printf("%d - %s \n", i, item.Task)
+	//	i++
+	// }
+
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done?"},
+			{Align: simpletable.AlignCenter, Text: "CreatedAt"},
+			{Align: simpletable.AlignCenter, Text: "CompletedAt"},
+		},
+	}
+
+	var cells [][]*simpletable.Cell
 	for i, item := range *t {
-		fmt.Printf("%d - %s \n", i, item.Task)
+		cells = append(cells, *&[]*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", i)},
+			{Text: item.Task},
+			{Text: fmt.Sprintf("%t", item.Done)},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: item.CompletedAt.Format(time.RFC822)},
+		})
 		i++
 	}
+
+	table.Body = &simpletable.Body{Cells: cells}
+	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+		{Align: simpletable.AlignCenter, Span: 5, Text: "Tasks here"},
+	}}
+
+	table.SetStyle(simpletable.StyleUnicode)
+	table.Println()
 }
